28f3e84db173a8e3fa5a0c1dbbd574be765ec2f5
prathima129 <prathima.shrestha@gmail.com>
2025-08-13 08:18:06 +0000

    Second

---
+++ server/app.js
@@ -0,0 +1,17 @@
+console.log("Hello World")
+
+let fname="Prathima";
+let lname="Shrestha";
+console.log(fname+lname);
+
+/*Data Types
+1.Primitive==>number,string,boolean,null,undefined
+2.Non-Primitive==>objects and Arrays
+*/
+let a=10;
+let b="Prathima"
+let c=false
+let d=null;
+let e=undefined
+
+let person=["Prathima",20,true,null,undefined]
